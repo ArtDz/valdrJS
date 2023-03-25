c70374068549b7796968975e516510b600cb598d
ArtDz <dzugaev1998@bk.ru>
2023-03-25 17:28:57 +0300

    some changes in js file

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,20 @@
 'use strict'
 
-console.log('arr' + ' - objext');
+console.log('arr' + ' - object');
 
 let incr = 10, decr = 11
 
-//
-
-console.log(false && true);
 console.log(incr || decr);
+
+const x = {
+    a: incr,
+    b: decr,
+    c: incr + decr
+}
+
+console.log(x.a * x.b)
+
+const y = (num) => {
+    return num * 22
+}
+console.log(y(77))
